5bbb51159b0bf5223fab09342944d73d080d795c
samriddhist <tripathisamriddhi@gmail.com>
2025-09-25 11:04:32 +0200

    Delete 1_About.py

--- pages/1_About.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.header("About this Project")
-st.markdown(
-        """
-        This tab contains information about the dataset, references, group members and contact information.
-
-        """)
