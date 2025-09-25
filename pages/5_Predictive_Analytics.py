1b1d68853609f054fe1ae6057eb4825510662904
samriddhist <tripathisamriddhi@gmail.com>
2025-09-25 12:13:11 +0200

    Update 5_Predictive_Analytics.py

--- pages/5_Predictive_Analytics.py
+++ pages/5_Predictive_Analytics.py
@@ -1,6 +1,22 @@
 import streamlit as st
 
-st.header("Predictive Analytics")
+st.set_page_config(
+    page_title="Predictive Analytics",layout="wide")
+
+st.sidebar.success("Select a tab above.")
+
+st.sidebar.image("./assets/Colorectal Cancer Logo.png",)
+
+st.markdown("""
+    <h1 style="
+        font-size: 60px;
+        font-weight: 700;
+        text-align: center;
+        color: #1261B5;
+    ">
+        Predictive Analytics
+    </h1>
+""", unsafe_allow_html=True)
 
 st.markdown(
 
