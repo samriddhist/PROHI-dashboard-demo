486c4187bdd2d41f563658b623f6e6f261e4882c
samriddhist <tripathisamriddhi@gmail.com>
2025-09-25 10:55:09 +0200

    Create 1_About.py

---
+++ pages/1_About.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.header("About this Project")
+st.markdown(
+        """
+        This tab contains information about the dataset, references, group members and contact information.
+
+        """)
