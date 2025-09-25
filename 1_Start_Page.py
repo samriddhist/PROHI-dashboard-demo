40b11dc2af4560146d7d26d60bbf814e2567a028
samriddhist <tripathisamriddhi@gmail.com>
2025-09-25 11:04:36 +0200

    Create 1_Start_Page.py

---
+++ 1_Start_Page.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Predicting Colorectal Cancer Survivability Dashboard",layout="wide")
+
+st.sidebar.success("Select a tab above.")
+
+st.write("# Colorectal Cancer Global Dataset & Predictions")
