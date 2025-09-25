9407a0492e9f4a759dc7c7a5c1a4d886e2dfa535
samriddhist <tripathisamriddhi@gmail.com>
2025-09-25 12:05:29 +0200

    Update 2_About.py

--- pages/2_About.py
+++ pages/2_About.py
@@ -1,8 +1,39 @@
 import streamlit as st
 
 st.header("About this Project")
-st.markdown(
-        """
-        This tab contains information about the dataset, references, group members and contact information.
 
-        """)
+with st.container():
+    st.write("""Dataset Infromation 
+
+             The link to the dataset is: https://www.kaggle.com/datasets/ankushpanday2/colorectal-cancer-global-dataset-and-predictions/data
+             It includes 28 features..
+
+""")
+
+    st.write("""Group Members
+             
+             Amanuel Teklehaimanot
+                Contact Information: 
+                        Email:
+             
+             Carlos Etornam Adabe
+                Contact Information: 
+                        Email:
+            
+              Dharinisri Magudapathy Saravanakumar
+                Contact Information: 
+                        Email:
+            
+              Jakob Lyckström
+                Contact Information: 
+                        Email:
+             
+             Samriddhi Tripathi 
+                Contact Information: 
+                        Email:tripathisamriddhi@gmail.com
+             
+             """)
+
+    st.write("""References 
+
+""")
