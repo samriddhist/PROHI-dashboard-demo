4aba5b29e191457b9fa3770a7d01e4ba84efeed3
samriddhist <tripathisamriddhi@gmail.com>
2025-09-25 11:40:52 +0200

    Update 1_Start_Page.py

--- 1_Start_Page.py
+++ 1_Start_Page.py
@@ -5,9 +5,24 @@ st.set_page_config(
 
 st.sidebar.success("Select a tab above.")
 
-st.title("# Colorectal Cancer Global Dataset & Predictions")
+st.sidebar.image("./assets/Colorectal Cancer Logo.png",)
+
+st.markdown("""
+    <h1 style="
+        font-size: 60px;
+        font-weight: 700;
+        text-align: center;
+        color: #1261B5;
+    ">
+    Colorectal Cancer Global Dataset & Predictions
+    </h1>
+""", unsafe_allow_html=True)
 
 with st.container():
-    st.write("This is inside the container")
+    st.write("What is colorectal cancer?")
+    st.write("What is it important to investigate?")
+    st.write("What can you find on this dashboard?")
+
+
 
 
