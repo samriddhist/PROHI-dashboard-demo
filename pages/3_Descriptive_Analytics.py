5f014359fc09180c784b2713c377b412833b36a2
samriddhist <tripathisamriddhi@gmail.com>
2025-09-25 12:13:07 +0200

    Update 3_Descriptive_Analytics.py

--- pages/3_Descriptive_Analytics.py
+++ pages/3_Descriptive_Analytics.py
@@ -1,6 +1,22 @@
 import streamlit as st
 
-st.header("Descriptive Analytics")
+st.set_page_config(
+    page_title="Descriptive Analytics",layout="wide")
+
+st.sidebar.success("Select a tab above.")
+
+st.sidebar.image("./assets/Colorectal Cancer Logo.png",)
+
+st.markdown("""
+    <h1 style="
+        font-size: 60px;
+        font-weight: 700;
+        text-align: center;
+        color: #1261B5;
+    ">
+        Descriptive Analytics
+    </h1>
+""", unsafe_allow_html=True)
 
 st.markdown(
 
