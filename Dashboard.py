e24cbc39cebfc4378fe58a6055c423b48cc65387
samriddhist <tripathisamriddhi@gmail.com>
2025-09-25 11:04:33 +0200

    Delete Dashboard.py

--- Dashboard.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.set_page_config(
-    page_title="Predicting Colorectal Cancer Survivability Dashboard",layout="wide")
-
-st.sidebar.success("Select a tab above.")
-
-st.write("# Colorectal Cancer Global Dataset & Predictions")
