88077f64eacaa6ac14dc6060bebea8e40f7aba3f
samriddhist <tripathisamriddhi@gmail.com>
2025-10-16 14:21:05 +0200

    Update 6_Prescriptive_Analytics.py

--- pages/6_Prescriptive_Analytics.py
+++ pages/6_Prescriptive_Analytics.py
@@ -6,6 +6,14 @@ import os
 import numpy as np
 
 st.set_page_config(page_title="Prescriptive Analytics", layout="wide")
+st.sidebar.success("Select a tab above.")
+st.sidebar.image("./assets/Colorectal Cancer Logo.png")
+
+st.markdown("""
+<h1 style="font-size:60px; font-weight:700; text-align:center; color:#1261B5;">
+Prescriptive Analytics
+</h1>
+""", unsafe_allow_html=True)
 
 model = None
 preprocessor = None
